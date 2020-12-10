93f37f189f120f0a9e6c77a8cc34e5c471a5f70d
Tu Anh Huynh <tuanhlyhuynh@gmail.com>
2020-12-10 12:11:05 -0500

    [FruitsContainer] Add class and import compoents

---
+++ src/components/FruitContainer.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+// Components
+import List from './List';
+import Input from './Input';
+
+class FruitContainer extends Component {
+    constructor (props) {
+        super()
+        this.state = {
+            // initialize the fruit list to the full list passed into props
+            fruitsToDisplay: this.props.fruits,
+            // initialize the filter value to an empty string
+            filterValue: '',
+
+        }
+    }
+
+    handleFilterChange = (e) => {
+        e.preventDefault();
+        let filterValue = e.target.value;
+        // Remove the fruits that don't contain filter value
+        const filteredFruitList = this.props.fruits.filter(fruit => {
+            return fruit.toLowerCase().includes(filterValue.toLowerCase());
+        })
+        this.setState({ fruitsToDisplay: filteredFruitList, filterValue})
+    }
+
+    render() {
+        // Inside of Input Component, to access props, I will say props.value
+        // Inside of List Component, to access props, I will say props.fruits
+        return (
+            <div>
+                
+                    <Input value={this.state.filterValue} onChange={this.handleFilterChange}/>
+                    <List fruits={this.state.fruitsToDisplay}/>
+                
+            </div>
+        )
+    }
+}
+
+export default FruitContainer;
